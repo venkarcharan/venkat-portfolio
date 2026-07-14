d77901729746d7b7fd2bb12fcf02f73c7a65679f
Venkat Charan <srvenkatcharan@gmail.com>
2026-07-14 15:03:48 +0530

    aded-base

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,25 +1,25 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "../components/Layout/Layout";
 import Home from "../pages/Home/Home";
-import Skills from "../pages/Skills/Skills";
-import Education from "../pages/Education/Education";
 import Projects from "../pages/Projects/Projects";
 import Contact from "../pages/Contact/Contact";
+import Education from "../pages/Education/Education";
+import Skills from "../pages/Skills/Skills";
 
 function AppRouter() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/venkat-portfolio">
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="education" element={<Education />} />
+          <Route path="skills" element={<Skills />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
